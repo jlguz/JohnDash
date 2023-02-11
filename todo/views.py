17b9d7cc65a355591b82e5602f8705eca6397b01
JohnGuzman <jlg05@hotmail.co.uk>
2023-02-11 22:16:00 +0000

    app fianlizada

--- todo/views.py
+++ todo/views.py
@@ -18,4 +18,25 @@ def index(request):
 
     return render(request,'todo/task_list.html', context)
 
+def updateTask(request,pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
 
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+    return render(request, 'todo/update_task.html',context)    
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+    context = {'item': item}
+    return render(request, 'todo/delete.html',context)  
